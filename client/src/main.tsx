0fa1dbf24a5629dae5d005dfa77ea6166f18990c
“Profana92” <stevevtek@gmail.com>
2023-08-11 14:23:26 +0200

    feat: 🎸 React Helmet added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { HelmetProvider } from 'react-helmet-async'
+import Home from './routes/pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <Home />
+      <h1 className="text-center text-red-500 h-full">hello</h1>
+    </HelmetProvider>
   </React.StrictMode>,
 )
